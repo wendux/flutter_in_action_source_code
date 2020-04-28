6e56d50f37b55ae83eb8b07ff23752caf5770622
wendux <824783146@qq.com>
2020-04-28 15:58:39 +0800

    更新示例

--- github_client_app/ios/Flutter/flutter_export_environment.sh
+++ github_client_app/ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,11 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/duwen/Documents/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/duwen/Documents/code/flutter_in_action/github_client_app"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/duwen/Documents/code/flutter_in_action/github_client_app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_FRAMEWORK_DIR=/Users/duwen/Documents/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
